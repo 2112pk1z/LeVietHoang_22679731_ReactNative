e2c7b26fdec9809644440f272d15369b1ad14cb3
H82M19\Student <Student>
2025-08-30 13:26:30 +0700

    bai 8

---
+++ Tuan02/dist/bai8.js
@@ -0,0 +1,17 @@
+"use strict";
+Promise.resolve(2)
+    .then(num => {
+    console.log("Start:", num);
+    return num * num;
+})
+    .then(num => {
+    console.log("After square:", num);
+    return num * 2;
+})
+    .then(num => {
+    console.log("After double:", num);
+    return num + 5;
+})
+    .then(result => {
+    console.log("Final result:", result);
+});
