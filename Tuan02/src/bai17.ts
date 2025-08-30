2c6c8568a22ab908a19a75239914768af3d85769
H82M19\Student <Student>
2025-08-30 14:13:01 +0700

    bai 17

---
+++ Tuan02/src/bai17.ts
@@ -0,0 +1,13 @@
+async function demoForAwait() {
+  const promises = [
+    Promise.resolve(1),
+    Promise.resolve(2),
+    Promise.resolve(3),
+  ];
+
+  for await (const value of promises) {
+    console.log(value);
+  }
+}
+
+demoForAwait();
