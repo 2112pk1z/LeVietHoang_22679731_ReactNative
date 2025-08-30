e28d459a9a3cab94029bceece14d255068e0268a
H82M19\Student <Student>
2025-08-30 14:34:38 +0700

    bai 22

---
+++ Tuan02/dist/bai22.js
@@ -0,0 +1,14 @@
+"use strict";
+async function getDatas() {
+    try {
+        for (let i = 1; i <= 3; i++) {
+            let response = await fetch(`https://jsonplaceholder.typicode.com/todos/${i}`);
+            let data = await response.json();
+            console.log(`Data ${i}:`, data);
+        }
+    }
+    catch (error) {
+        console.error("Có lỗi:", error);
+    }
+}
+getDatas();
