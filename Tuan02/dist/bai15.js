b0fef9871d532c01f2db5ce6043aa0668a2d4522
H82M19\Student <Student>
2025-08-30 14:07:02 +0700

    bai 15

---
+++ Tuan02/dist/bai15.js
@@ -0,0 +1,17 @@
+"use strict";
+async function multiplyByTwo(num) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(num * 2);
+        }, 1000);
+    });
+}
+async function runSequential() {
+    const result1 = await multiplyByTwo(2);
+    console.log("Kết quả 1:", result1);
+    const result2 = await multiplyByTwo(4);
+    console.log("Kết quả 2:", result2);
+    const result3 = await multiplyByTwo(6);
+    console.log("Kết quả 3:", result3);
+}
+runSequential();
