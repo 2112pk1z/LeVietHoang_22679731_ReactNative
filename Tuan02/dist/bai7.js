61ee6f2f070b34fbeceee396d221e3fed031556a
H82M19\Student <Student>
2025-08-30 13:22:57 +0700

    bai 7

---
+++ Tuan02/dist/bai7.js
@@ -0,0 +1,13 @@
+"use strict";
+function task(ms) {
+    return new Promise(resolve => {
+        setTimeout(() => resolve("Task done in " + ms + " ms"));
+    });
+}
+Promise.race([
+    task(1000),
+    task(2000),
+    task(1500)
+]).then(result => {
+    console.log("Fastest Promise:", result);
+});
